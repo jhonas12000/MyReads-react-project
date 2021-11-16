2e1624c347da9a6060a851f20a083a9451b4ae88
Yonas Weldemichael <yonasweldemichael@Yonass-MacBook-Pro.local>
2021-11-16 10:48:38 -0800

    succesfully fetched books from the API

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import Home from './components/HomeComponent';
 import Search from './components/SearchComponent';
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    
+    books: []
+  }
+  
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+      console.log(this.state.books)
+    })
   }
 
   render() {
+    
     return (
       <div className="app">
         {this.state.showSearchPage ? (
